f449cc6e0151f570e7fa45b1b9efff100fcd6a59
Hieu Vu <88392320+HieuTVu@users.noreply.github.com>
2025-02-27 23:25:10 -0600

    fixes to make tool work

--- agent.py
+++ agent.py
@@ -6,12 +6,15 @@ from typing import List, TypedDict, Literal
 from tools import RetrievalTool
 from utils import get_llm
 from langchain.output_parsers import PydanticOutputParser
+from data_types import ALL_DATA_TYPES
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+
 
 ENV = "dev"
 
 
 class Data(BaseModel):
-    type: str = Field(..., description="The type of data, e.g. heart-rate, sleep, vitals, etc.")
+    type: str = Field(..., description=f"The type of data, must be exactly one of {ALL_DATA_TYPES}")
     value: int = Field(..., description="The value of the data, e.g. 120, 8 hours, etc.")
     unit: str = Field(..., description="The unit of the data, e.g. rate/min, hours, etc.")
     start_date: str = Field(..., description="The date of the data, e.g. 2024-01-04")
@@ -23,7 +26,7 @@ class Data(BaseModel):
 
 
 class RetrievalStep(BaseModel):
-    type: str = Field(..., description="The type of data to retrieve, e.g. heart-rate, sleep, vitals, etc.")
+    type: str = Field(..., description=f"The type of data to retrieve. Must be exactly one of {ALL_DATA_TYPES}")
     range: int = Field(..., description="The latest number of records to retrieve, e.g. 100, 14, etc.")
 
 
@@ -48,16 +51,20 @@ class FitAgentGraph:
     ):
         self.strategy: str = strategy
         # TODO: find a way to pass the LLM models
-        self.general_llm: OllamaLLM = kwargs.get("general_llm", get_llm())
-        self.medical_llm: OllamaLLM = kwargs.get("medical_llm", get_llm())
+        self.general_llm: OllamaLLM = get_llm(model="llama3")
+        self.medical_llm: OllamaLLM = get_llm(model="medllama2")
         self.stream = stream
+        self.tool = RetrievalTool(
+            llm=self.medical_llm,
+        )
 
     def describing_incoming_data(
         self,
         state: FitAgentState,
     ):
-        prompt: str = """
-You are a medical data analyst specializing in wearable health metrics. Your task is to analyze incoming health data and generate a detailed medical description of the provided measurements. The description should be precise, medically accurate, and free from hallucinations. Follow these guidelines:
+        print(f"Describing incoming data:\n")
+        template: str = """
+You are a medical data analyst specializing in describing wearable health metrics. Your task is to analyze incoming personal health and fitness data and generate a detailed description of the provided measurements. The description should be precise, medically accurate, and free from hallucinations. Follow these guidelines:
 1. Identify and describe the health data:
 - Clearly define the type of measurement (e.g., heart rate, sleep duration, respiratory rate, blood oxygen saturation, workout data, weight, height, etc.).
 - Include the value, unit of measurement, and the timestamp of the recorded data.
@@ -75,34 +82,41 @@ Example:
 5. Ensure neutrality and professionalism:
 - Do not include personal opinions.
 - Keep the explanation scientific and objective.
+- Be concise, straightforward and informative without unnecessary elaboration. You do not need to tell the user to consult with a healthcare professional.
+
+-----------------------------------------------------------
+Now, describe in details the incoming data: {incoming_data}
 """
-        touse_llm = self.general_llm if self.strategy == "general" else self.medical_llm
-        messages = [SystemMessage(prompt)] + [HumanMessage(f"Describe in details the incoming data: {state['incoming_data'].info}")]
+        prompt = ChatPromptTemplate.from_template(template)
+        touse_llm = prompt | self.medical_llm
         if self.stream:
             response = ""
             for chunk in touse_llm.stream(
-                messages
+                {"incoming_data": state["incoming_data"].info}
             ):
                 response += chunk
                 print(chunk, end="", flush=True)
             print(f"\n---\n")
         else:
             response = touse_llm.invoke(
-                messages
+                {"incoming_data": state["incoming_data"].info}
             )
         return {"incoming_data_description": response}
 
-    def making_retrieval_plan(
+    async def making_retrieval_plan(
         self,
         state: FitAgentState,
     ):
-        prompt: str = """
+        print(f"Making a retrieval plan:\n")
+        parser = PydanticOutputParser(pydantic_object=RetrievalPlan)
+        template: str = f"""
 You are an expert in medical AI tasked with retrieving relevant historical health data to enhance contextual analysis. Your goal is to formulate a retrieval plan that will complement the incoming data to provide deeper insights. Follow these structured steps:
 1. Understand the incoming data description:
 - Carefully review the provided health data (type, value, unit, timestamp) based on the incoming data description.
 2. Identify the necessary historical data:
 - Select which types of historical data would provide meaningful context for analysis (e.g., past heart rate trends, previous sleep records, weight history, etc.).
 - Consider whether the data type requires continuous tracking over multiple days (e.g., heart rate, sleep) or if the latest recorded value is sufficient (e.g., height, weight).
+- Note that the data type must be exactly as in this list: """ + str(ALL_DATA_TYPES) + """.
 3. Determine the appropriate retrieval range:
 - Define a suitable time window for historical data retrieval based on medical reasoning.
 - For time-series data (e.g. heart rate, sleep, vitals): Suggest a historical period that provides useful trend insights (e.g., past 14 days of sleep records, last 100 heart rate measurements).
@@ -119,45 +133,56 @@ You are an expert in medical AI tasked with retrieving relevant historical healt
 5. Safety and compliance:
 - Do not assume or predict a medical condition.
 - Ensure that the retrieval plan aligns with established health monitoring best practices.
-Output format: Clearly structured retrieval plan specifying data types and time ranges.
-"""
-        messages = [SystemMessage(prompt)] + [HumanMessage(f"Make a plan to retrieve the data to contextualize the incoming data: {state['incoming_data_description']}")]
-        touse_llm = self.general_llm if self.strategy == "general" else self.medical_llm
+- The user clearly understands this is just the suggestion. Thus, you do not need to tell the user to consult with a healthcare professional.
+- Be consise, straight to the point and informative without unnecessary elaboration.
+
+-----------------------------------------------------------
+Here is how to structure the output: {format_instructions}
+
+-----------------------------------------------------------
+Now, make a plan to retrieve the data to contextualize the incoming data:
+""" + " {incoming_data_description}"
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["incoming_data_description"],
+            partial_variables={"format_instructions": parser.get_format_instructions()},
+        )
+        touse_llm = prompt | self.general_llm | parser
+
+        response = await touse_llm.ainvoke(
+            {"incoming_data_description": state["incoming_data_description"]}
+        )
+
         if self.stream:
-            streamed_response = ""
-            for chunk in touse_llm.stream(
-                messages
-            ):
-                streamed_response += chunk
-                print(chunk, end="", flush=True)
+            for step in response.plan:
+                print(step)
             print(f"\n---\n")
-            parser = PydanticOutputParser(pydantic_object=RetrievalPlan)
-            response: RetrievalPlan = parser.parse(streamed_response)
-        else:
-            response: RetrievalPlan = touse_llm.with_structured_output(RetrievalPlan).invoke(
-                messages
-            )
+        
         return {"retrieval_plan": response.plan}
 
     def retrieving_data(
         self,
         state: FitAgentState,
     ):
+        print(f"Retrieving data:\n")
         outputs = []
         for step in state["retrieval_plan"]:
             print(f"\n---{step}---\n")
-            tool_output = RetrievalTool._run(step.type, step.range, describe=True)
+            tool_output = self.tool._run(step.type, step.range, describe=True)
             outputs.append(
                 tool_output
             )
-        return {"retrieved_data": outputs}
+        print(f"\n---\n")
+        return {"retrieved_data": "\n------------------------------\n".join(outputs)}
     
     def generating_insights(
         self,
         state: FitAgentState,
     ):
-        prompt: str = """
-You are a medical AI system responsible for generating insights from health data. Your analysis must be medically accurate, factual, and safe. Your tasks include trend analysis, anomaly detection, and providing actionable recommendations. Follow these structured guidelines:
+        print(f"Generating insights:\n")
+        template: str = f"""
+You are a medical AI system responsible for generating insights on the INCOMING health data. You must use the historical and related health metrics records retrieved from the database to contextualize and use as the background of the user current health status.
+Your analysis must be medically accurate, factual, and safe. Your tasks include trend analysis, anomaly detection, and providing actionable recommendations. Follow these structured guidelines:
 1. Trend Analysis:
 - Compare the incoming data against historical records.
 - Identify patterns or deviations (e.g., increased heart rate over the last week, decreasing sleep duration, stable weight trends, etc.).
@@ -175,7 +200,7 @@ You are a medical AI system responsible for generating insights from health data
 - Do not diagnose or provide medical treatment plans—only insights and general health guidance.
 
 Output Structure:
-- Trend Summary: A concise overview of observed trends.
+- Trend Summary: An overview of observed trends of the historical data and the incoming data.
 - Anomaly Detection: If applicable, a factual description of irregularities.
 - Insights & Possible Correlations: A medically relevant interpretation of findings.
 - General Wellness Recommendations: Practical and safe health tips without overstepping medical expertise.
@@ -185,20 +210,27 @@ Example Output:
 - Anomaly Detected: Today's resting heart rate is 15% higher than the recent average.
 - Insights: Sleep deprivation is a known factor in elevated heart rate, and the reduced sleep trend over the past two weeks may be influencing this change.
 - General Wellness Recommendations: Ensuring adequate sleep and hydration may help regulate heart rate. If the elevated heart rate persists or is accompanied by other symptoms, consulting a healthcare professional is recommended.
-"""
-        touse_llm = self.general_llm if self.strategy == "general" else self.medical_llm
-        messages = [SystemMessage(prompt)] + [HumanMessage("Generate insights from the retrieved data.")]
+
+-----------------------------------------------------------
+Here is the description of the historical and related health metrics:
+{state['retrieved_data']}
+-----------------------------------------------------------
+
+Now, Give me the insights of this incoming data: 
+""" + " {incoming_data_description}"
+        prompt = ChatPromptTemplate.from_template(template)
+        touse_llm = prompt | self.general_llm
         if self.stream:
             response = ""
             for chunk in touse_llm.stream(
-                messages
+                {"incoming_data_description": state["incoming_data_description"]}
             ):
                 response += chunk
                 print(chunk, end="", flush=True)
             print(f"\n---\n")
         else:
             response = touse_llm.invoke(
-                messages
+                {"incoming_data_description": state["incoming_data_description"]}
             )
         return {"insights": response}
 
@@ -258,9 +290,27 @@ class FitAgent:
         data = Data(
             **arguments
         )
-        response = self.graph.invoke(
+        response = await self.graph.ainvoke(
             {
                 "incoming_data": data
             }
         )
         return response.get("insights", "An error happened. Please try again.")
+
+
+if __name__ == "__main__":
+    agent = FitAgent(
+        strategy="general"
+    )
+    import asyncio
+    response = asyncio.run(
+        agent.run(
+            arguments={
+                "type": "heart-rate",
+                "value": 200,
+                "unit": "rate/min",
+                "start_date": "2025-02-27",
+                "end_date": "2025-02-27"
+            }
+        )
+    )
--- tools.py
+++ tools.py
@@ -1,56 +1,81 @@
-from langchain.tools import BaseTool
-from pydantic import BaseModel, Field
-import ollama
-from langchain_community.llms import Ollama
-from time import time
+from langchain_ollama.llms import OllamaLLM
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 import pytz
+import pandas as pd
 
-class RetrievalTool(BaseTool):
-    name: str = ""
-    description: str = ""
+
+def read_xml_data(
+    file_path: str = "./export.xml"
+) -> pd.DataFrame:
+    # record_data = pd.read_csv("record_data.csv")
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    record_list = [x.attrib for x in root.iter('Record')]
+    record_data = pd.DataFrame(record_list)
+    record_data = record_data.drop(columns=["sourceName", "sourceVersion", "creationDate", "device"])
+    record_data.dropna(subset=["unit"], inplace=True)
+    record_data['type'] = record_data['type'].str.replace('HKQuantityTypeIdentifier', '').replace('HKDataType', '').replace('HKCategoryTypeIdentifier', '')
+    record_data.to_csv("record_data.csv", index=False)
+    return record_data
+
+class RetrievalTool:
+    name: str = "retrieva_tool"
+    description: str = "A tool to retrieve data from an XML file and generate a description of the data using an LLM."
     llm: OllamaLLM
 
     def __init__(
         self,
-        llm_name,
-        file_path
+        llm: OllamaLLM,
+        file_path: str = "./export.xml"
     ):
-
-        # Define llm and preprocess the element tree here
-        self.llm = Ollama(model=llm_name)
-        tree = ET.parse(file_path)
-        self.root = tree.getroot()
-
+        self.llm = llm
+        self.record_data = read_xml_data(file_path)
+        
+    def get_all_data_types(self) -> list:
+        return self.record_data['type'].unique().tolist()
 
     def _run(
         self,
         data_type: str,
         most_recent_value: int,
-        describe: bool = False
+        describe: bool = False,
+        stream: bool = True
     ) -> str:
-        pass
-        # Retrieve the data from the element tree
-        # Call the LLM to generate a description on the data (don't do summarization, we need as much data as possible)
-        # Return the description
-        data_input = []
-        for record in self.root.findall("Record"):
-            if  record.get("type") == data_type :
-                date = record.get("startDate")
-                date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z") 
-                now = datetime.now(pytz.utc)
-                time_days_ago = now - timedelta(days=most_recent_value)
-                if date >= time_days_ago:
-                    data_input += f'Data Type: {record.get('type')}, Date of data: {record.get('startDate')}, Data value: {record.get('value')}.|'
-        prompt_input = (f"Analyze the following dataset for the data type '{data_type}'. "
-                f"The data spans the last {most_recent_value} days. "
-                "Provide a concise summary, key trends, and any notable patterns. "
-                "Data entries are formatted as 'Type, Date, Value'. "
-                f"Here is the data: {data_input}")
-        response = self.llm.invoke(prompt_input)
-        return prompt_input
+        records = self.record_data[self.record_data["type"] == data_type].sort_values(by=["endDate"], ascending=[False]).head(most_recent_value).to_markdown()
+
+        # if describe:
+        #     prompt_input = (f"Analyze the following dataset for the data type '{data_type}'. "
+        #         f"The data spans the last {most_recent_value} days. "
+        #         "Provide a concise summary, key trends, and any notable patterns. "
+        #         "Data entries are formatted as 'Type, Date, Value'. "
+        #         f"Here is the data: {data_input}")
+        
+        #     if stream:
+        #         response = ""
+        #         for chunk in self.llm.stream(prompt_input):
+        #             response += chunk
+        #             print(chunk, end="", flush=True)
+        #         print()
+        #     else:
+        #         response = self.llm.invoke(prompt_input)
+        #     return response
+        
+        return records
 
 
-                
-                
+if __name__ == "__main__":
+    from utils import get_llm
+    tool = RetrievalTool(
+        llm=get_llm(),
+        file_path="./export.xml"
+    )
+
+    data = tool._run(
+        data_type="SleepDuration",
+        most_recent_value=14,
+        describe=False,
+        stream=False
+    )
+
+    print(data)
