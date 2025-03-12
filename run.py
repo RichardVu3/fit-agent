de696527db67a5d970fab3874ec995ec31f36c8a
Richard Vu <88392320+RichardVu3@users.noreply.github.com>
2025-03-12 17:35:57 -0500

    Update run.py

--- run.py
+++ run.py
@@ -34,15 +34,6 @@ def main():
     if ENV == "prod":
         print(response)
 
-def judge_response(judge_name, response_from_agent):
-    llm_judge = OllamaLLM(model=judge_name)
-    prompt_input = f"You are an LLM judge responsible for evaluating the output of a health agent. The health agent generates health suggestions or concern alerts based on the provided input data. \
-    Your task is to fairly and objectively assess the quality of the agent's response by considering its accuracy, relevance, completeness, and appropriateness given the input data. Provide a single numerical score from 0 to 10, where:\
-    0 = Completely incorrect or misleading response and 10 = Fully accurate, relevant, and appropriate response. Here is the agent's response for evaluation: " + response_from_agent
-    response = llm_judge.invoke(prompt_input)
-    return response
-
-
 
 if __name__ == "__main__":
     main()
